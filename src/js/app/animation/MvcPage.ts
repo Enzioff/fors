d25717d4946c8326a2c19de82bead7f4a62e469c
Enzioff <kavlod12345@gmail.com>
2024-10-31 10:57:01 +0300

    feat: add decision page

--- src/js/app/animation/MvcPage.ts
+++ src/js/app/animation/MvcPage.ts
@@ -205,86 +205,88 @@ export class MvcPage extends AnimationConfig {
     }
 
     protected cardsSection = () => {
-        const section = document.querySelector('.trigger-slim-cards');
-        if (!section) return;
+        const sections = document.querySelectorAll('.trigger-slim-cards');
+        if (!sections) return;
+        sections.forEach(section => {
 
-        const listItems = document.querySelectorAll('.trigger-slim-cards .article-info');
-        const container = section.querySelector('.layering');
-        const shadow = container.querySelector('.layering__shadow');
-        const pinnedBlock = section.querySelector('.anim-stay')
-        const shadowHeight = shadow.getBoundingClientRect().height;
+            const listItems = section.querySelectorAll('.article-info');
+            const container = section.querySelector('.layering');
+            const shadow = container.querySelector('.layering__shadow');
+            const pinnedBlock = section.querySelector('.anim-stay')
+            const shadowHeight = shadow.getBoundingClientRect().height;
 
-        const cardsTimeLine = gsap.timeline({
-            scrollTrigger: {
-                trigger: container,
-                start: 'center center',
-                end: () => `+=${listItems?.length * (listItems[0]?.getBoundingClientRect().height + 200)}`,
-                scrub: 1,
-                onEnter: self => self.refresh()
-            }
-        });
-
-        this.mm.add({
-            isDesktop: `(min-width: ${this.breakPoints.desktop}px)`,
-            isTabletMax: `(max-width: ${this.breakPoints.tabletMax}px)`,
-            isTablet: `(min-width: ${this.breakPoints.tablet}px)`,
-            isMobileMax: `(max-width: ${this.breakPoints.mobileMax}px)`,
-        }, (context) => {
-            const {isDesktop, isTabletMax, isTablet, isMobileMax} = context.conditions;
-
-            const calcHeight = (idx: number) => {
-                if (isDesktop) {
-                    return shadowHeight - (idx * 25)
-                } else if (isTablet) {
-                    return shadowHeight - (idx * 11)
-                } else if (isMobileMax) {
-                    return shadowHeight - (idx * 11)
+            const cardsTimeLine = gsap.timeline({
+                scrollTrigger: {
+                    trigger: container,
+                    start: 'center center',
+                    end: () => `+=${listItems?.length * (listItems[0]?.getBoundingClientRect().height + 200)}`,
+                    scrub: 1,
+                    onEnter: self => self.refresh()
                 }
-            }
+            });
 
-            const resizeShadow = (idx: number = 0): void => {
-                gsap.to(shadow, {
-                    height: calcHeight(idx),
-                    duration: 0.2,
-                })
-            }
+            this.mm.add({
+                isDesktop: `(min-width: ${this.breakPoints.desktop}px)`,
+                isTabletMax: `(max-width: ${this.breakPoints.tabletMax}px)`,
+                isTablet: `(min-width: ${this.breakPoints.tablet}px)`,
+                isMobileMax: `(max-width: ${this.breakPoints.mobileMax}px)`,
+            }, (context) => {
+                const {isDesktop, isTabletMax, isTablet, isMobileMax} = context.conditions;
 
-            ScrollTrigger.create({
-                trigger: pinnedBlock,
-                pin: true,
-                pinSpacing: true,
-                scrub: 1,
-                start: 'center center',
-                end: () => `+=${listItems?.length * (listItems[0]?.getBoundingClientRect().height + 200) + 300}`,
-                onToggle: self => {
-                    self.refresh()
-                    if (self.isActive) {
-                        if (isMobileMax) {
-                            this.moveCanvas(0, {yPercent: -20})
-                        }
-                    } else {
-                        if (isMobileMax) {
-                            this.moveCanvas(0, {yPercent: 0})
-                        }
+                const calcHeight = (idx: number) => {
+                    if (isDesktop) {
+                        return shadowHeight - (idx * 25)
+                    } else if (isTablet) {
+                        return shadowHeight - (idx * 11)
+                    } else if (isMobileMax) {
+                        return shadowHeight - (idx * 11)
                     }
-                },
-            })
+                }
 
-            listItems.forEach((card, idx) => {
-                const isFirstCard = idx === 0;
-                const animationIdx = idx + 3;
+                const resizeShadow = (idx: number = 0): void => {
+                    gsap.to(shadow, {
+                        height: calcHeight(idx),
+                        duration: 0.2,
+                    })
+                }
 
-                cardsTimeLine.from(card, {
-                    y: () => idx === 0 ? 0 : card.getBoundingClientRect().height + (idx === 1 ? 0 : 100),
-                    duration: 1,
-                    ease: 'linear',
-                    onStart: () => {
-                        resizeShadow(idx)
-                        animateSpline(this.application, 10 + idx)
+                ScrollTrigger.create({
+                    trigger: pinnedBlock,
+                    pin: true,
+                    pinSpacing: true,
+                    scrub: 1,
+                    start: 'center center',
+                    end: () => `+=${listItems?.length * (listItems[0]?.getBoundingClientRect().height + 200) + 300}`,
+                    onToggle: self => {
+                        self.refresh()
+                        if (self.isActive) {
+                            if (isMobileMax) {
+                                this.moveCanvas(0, {yPercent: -20})
+                            }
+                        } else {
+                            if (isMobileMax) {
+                                this.moveCanvas(0, {yPercent: 0})
+                            }
+                        }
                     },
-                    onReverseComplete: () => {
-                        resizeShadow(!isFirstCard ? idx - 1 : idx)
-                    }
+                })
+
+                listItems.forEach((card, idx) => {
+                    const isFirstCard = idx === 0;
+                    const animationIdx = idx + 3;
+
+                    cardsTimeLine.from(card, {
+                        y: () => idx === 0 ? 0 : card.getBoundingClientRect().height + (idx === 1 ? 0 : 100),
+                        duration: 1,
+                        ease: 'linear',
+                        onStart: () => {
+                            resizeShadow(idx)
+                            animateSpline(this.application, 10 + idx)
+                        },
+                        onReverseComplete: () => {
+                            resizeShadow(!isFirstCard ? idx - 1 : idx)
+                        }
+                    })
                 })
             })
         })
